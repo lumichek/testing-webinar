4c263fbb6bf758d0130efb47080ac5405dc291f6
Anna CHernoskutova <a.p.chernoskutova@esoft.local>
2022-05-12 15:45:21 +0500

    Init step 1 - jest hooks

--- src/__tests__/mock.test.js
+++ src/__tests__/mock.test.js
@@ -1,6 +1,3 @@
-import { register } from "../utils/auth-api";
-import { AUTH_SERVER_URL } from "../utils/constants";
-
 describe('check register function', () => {
   beforeEach(() => {
     jest.spyOn(global, 'fetch').mockResolvedValue({
@@ -12,28 +9,5 @@ describe('check register function', () => {
   });
   afterEach(() => {
     jest.restoreAllMocks();
-  });
-
-  test('should be success', async() => {
-    const result = await register('email', 'password');
-    expect(result).toEqual({data: ['test']});
-    expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith(`${AUTH_SERVER_URL}/signup`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({email: 'email', password: 'password'})
-    });
-  });
-
-  test('should be failed', async() => {
-    fetch.mockImplementationOnce(() => Promise.resolve({
-        ok: false,
-        status: '500',
-    }));
-
-    await expect(register('email', 'password')).rejects.toBe('Ошибка: 500');
   });
 })
