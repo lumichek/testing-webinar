3465e55ab2711d8cae2f9458bce75dddc93b2efc
Anna CHernoskutova <a.p.chernoskutova@esoft.local>
2022-05-12 15:39:33 +0500

    Init step 5 - redux

--- src/__tests__/redux.test.js
+++ src/__tests__/redux.test.js
@@ -1,23 +1,7 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import {ActionTypes, register} from '../store/auth/actions';
+import {ActionTypes} from '../store/auth/actions';
 import reducer from '../store/auth/reducer';
-import * as authApi from "../utils/auth-api";
 
 describe('check redux store and actions', () => {
-  beforeEach(() => {
-    jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue(
-        { result: 'OK' }
-      ),
-      ok: true,
-    });
-  });
-
-  afterEach(() => {
-      jest.restoreAllMocks();
-  });
-
   test('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(
       {
@@ -48,28 +32,5 @@ describe('check redux store and actions', () => {
         registerSending: false
       }
     );
-  });
-
-  test('should fire 3 actions after register is dispatched', () => {
-    const middlewares = [thunk.withExtraArgument({authApi})]
-    const mockStore = configureMockStore(middlewares)
-
-    fetch.mockImplementationOnce(() => Promise.resolve({
-      ok: true,
-      json: () => Promise.resolve({ success: 'OK' }),
-    }));
-
-    const expectedActions = [
-      {type: ActionTypes.SET_REGISTER_SENDING, payload: true},
-      {type: ActionTypes.SET_REGISTER_SEND_ERROR, payload: ''},
-      {type: ActionTypes.SET_REGISTER_SENDING, payload: false},
-    ];
-
-    const store = mockStore({});
-
-    return store.dispatch(register({email: 'email', password: 'password'}))
-      .then(() => {
-        expect(store.getActions()).toEqual(expectedActions)
-      });
   });
 });
